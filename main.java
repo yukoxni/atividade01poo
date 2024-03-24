04faaafa165111edd53662e1e22f1c49af62b9f3
yukoxni <163897204+yukoxni@users.noreply.github.com>
2024-03-23 21:21:41 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,17 @@
-package atividade01;
-
 public class Main {
-    public static void main (String[] args ) {
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora(10, 5);
 
-        Calculadora calc = new Calculadora();
-        calc.numA = 2;
-        calc.numB = 3;
-        System.out.println("Soma: " + calc.somar ());
-        System.out.println("Subtrair: " + calc.subtrair());
-        System.out.println("Multiplicar: " + calc.multiplicar());
-        System.out.println("Dividir: " + calc.dividir());
-        System.out.println("0:" + calc.verificarNumeroPrimo(0));
-        System.out.println("1: " + calc.verificarNumeroPrimo(1));
-        System.out.println("2: "+ calc.verificarNumeroPrimo(2));
-        
+        System.out.println("Soma: " + calc.somar());
+        System.out.println("Subtracao: " + calc.subtrair());
+        System.out.println("Multiplicacao: " + calc.multiplicar());
+        System.out.println("Divisao: " + calc.dividir());
+
+        int num = 17;
+        if (calc.verificarNumeroPrimo(num)) {
+            System.out.println(num + " e um numero primo.");
+        } else {
+            System.out.println(num + " nao e um numero primo.");
         }
-    
+    }
 }
